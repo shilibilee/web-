6af83a83cd0e6103aa313b01f6fc2409a884f92f
Aktaruzzaman Siddiquei <131073700+shilibilee@users.noreply.github.com>
2024-06-11 20:25:42 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middlewear
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1qll9ix.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const toyCollection = client.db('turboSporty').collection('toys');
+
+    app.get('/toys', async (req, res) => {
+      let query = {};
+      if (req.query?.seller_email) {
+        query = { seller_email: req.query.seller_email }
+      }
+      const sort = req.query?.sort ? parseInt(req.query.sort) : 1; // Default to ascending order
+      const cursor = toyCollection.find(query).sort({ price: sort });
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/toys/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await toyCollection.findOne(query);
+      res.send(result);
+    })
+
+    app.post('/toys', async (req, res) => {
+      const toys = req.body;
+      const result = await toyCollection.insertOne(toys);
+      res.send(result);
+    })
+
+    app.delete('/toys/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toyCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    app.put('/toys/:id', async(req,res) =>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const options = { upsert: true };
+      const updatedToy = req.body;
+      const toy = {
+        $set: {
+          price: updatedToy.price,
+          quantity: updatedToy.quantity,
+          description: updatedToy.description
+        },
+      }
+      const result = await toyCollection.updateOne(filter, toy, options)
+      res.send(result);
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Turbo Sporty Toy is running!')
+})
+
+app.listen(port, () => {
+  console.log(`Turbo Sporty Toy is running on port ${port}`)
+})
